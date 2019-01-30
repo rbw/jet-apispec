186986fe6d5ea2a021bb647a62239c0afe64f604
Robert Wikman <rbw@vault13.org>
2019-01-30 10:39:48 +0100

    pkg -> __jetpack__

--- jet_apispec/__init__.py
+++ jet_apispec/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from jetfactory import Jetpack
-from jet_apispec.controller import Controller
-from jet_apispec.services import svc_pkgs
+from .controller import Controller
+from .services import svc_pkgs
 
-pkg = Jetpack(
+__jetpack__ = Jetpack(
     name='apispec',
     description='OpenAPI specification generator',
     controller=Controller,
